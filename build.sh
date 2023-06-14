118e3859acc41e0b9269f3540122c8569ec8da82
Aesistril <yigitayaz262@gmail.com>
2023-06-14 23:43:28 +0300

    added logging

--- build.sh
+++ build.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 
+printf "Copying static files... "
 # Delete and create ./build/
 rm -rf ./build
 mkdir ./build
-mkdir ./build/temp
 
 # Copy static files
 cp -r ./files ./build
 cp -r ./template/css ./build
+printf "DONE!"
 
-# mustache ./content/base.yaml ./content/index.yaml 
-# mustache ./test.yaml ./template/base.mustache > ./build/index.html
+printf "\nBuilding pages with mustache... "
 mustache ./content/base.yaml ./content/index.yaml | mustache - ./template/base.mustache > ./build/index.html
-
-# Cleanup
-rm -rf ./build/temp
+printf "DONE!"
+printf "\nBuild Completed!\n"
