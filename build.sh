6dcc19b7a5d17eafe82972a8d53c1042bcdcfbb8
Aesistril <yigitayaz262@gmail.com>
2023-06-15 10:05:24 +0300

    Blog index page. WIP

--- build.sh
+++ build.sh
@@ -4,6 +4,7 @@ printf "Copying static files... "
 # Delete and create ./build/
 rm -rf ./build
 mkdir ./build
+mkdir ./build/blog
 
 # Copy static files
 cp -r ./files ./build
@@ -11,6 +12,7 @@ cp -r ./template/css ./build
 printf "DONE!"
 
 printf "\nBuilding pages with mustache... "
-mustache ./content/base.yaml ./content/index.yaml | mustache - ./template/base.mustache > ./build/index.html
+mustache ./content/extensions.yaml/base.yaml ./content/index.yaml | mustache - ./template/base.mustache > ./build/index.html
+mustache ./content/extensions.yaml/base.yaml ./content/blog/index.yaml | mustache - ./template/blog/index.mustache > ./build/blog/index.html
 printf "DONE!"
 printf "\nBuild Completed!\n"
